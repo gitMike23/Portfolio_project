dc4e15f619697c38e8c6221dbe10aa1312485ee6
Mikhail Medovnik <medovnikfl@gmail.com>
2020-11-16 21:45:22 +0300

    second section developed

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,8 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const menuClose = document.querySelector('.menu__close');
 
+
+
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
